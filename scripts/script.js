6550105a8482f4b6db4b944b00d03d8d08c3b956
VirtualVince <vincentm_2@hotmail.com>
2025-08-19 18:49:13 +0000

    sidebar menu function added. Scrolling hidden/navbar function added. BUG: navbar hides while scrolling down although sidebar menu is open, closing sidebar not possible without the navbar.

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,34 @@ hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
 })
+
+{
+    const nav = document.querySelector(".navbar");
+    let lastScrollY = window.scrollY;
+
+    let menu = document.querySelector(".off-screen-menu");
+    let isMenuOpen = menu.classList.contains('active');
+
+    window.addEventListener('scroll', () => {
+        if (lastScrollY < window.scrollY) {
+            console.log(isMenuOpen)
+            nav.classList.add("nav-hidden");
+            console.log("going down");
+            console.log(lastScrollY)
+
+        } else {
+
+            nav.classList.remove("nav-hidden");
+            console.log("going up");
+            console.log(lastScrollY)
+
+        }
+    })
+
+    lastScrollY = window.scrollY;
+    console.log(lastScrollY)
+
+    if (window.scrollY <= 0 || isMenuOpen) {
+        nav.classList.remove("nav-hidden");
+    }
+}
